c3313e7fe779e9d238c71946d128d3ec4d03f478
Kashif <nizamanikashif2003@gmail.com>
2025-06-12 21:09:27 +0500

    displayed books on page and added a button for adding new books

--- Library/script.js
+++ Library/script.js
@@ -15,8 +15,27 @@ function Book(name, author, no_of_pages, isRead){
     library.push(book);
  }
 
- addBooktoLibrary("hobbit", "tolkien", 300, false);
- addBooktoLibrary("1984", "george orwell", 425, true);
- addBooktoLibrary("Shadow of the wind", "Carlos Luis Zafon", 350, true)
+ addBooktoLibrary("Lord of the Rings", " J.R.R Tolkien", 300, false);
+ addBooktoLibrary("1984", "George Orwell", 425, true);
+ addBooktoLibrary("Shadow of the Wind", "Carlos Luis Zafon", 350, true)
 
  console.log(library)
+
+ function displayBooks(){
+
+    const container = document.querySelector(".container")
+
+    for(let i = 0; i < library.length; i++){
+
+        const div = document.createElement("div");
+        div.classList.add("book");
+        div.innerHTML = "<h1>" + library[i].name + "</h1> by " + library[i].author + ". pages: " + library[i].no_of_pages;
+        
+
+        container.appendChild(div);
+        
+
+    }
+ }
+
+ displayBooks();
