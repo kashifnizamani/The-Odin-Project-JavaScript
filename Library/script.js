96b175917140925b0c00da743f090d57e1d88675
Kashif <nizamanikashif2003@gmail.com>
2025-06-12 16:57:06 +0500

    made the constructor and a function to create and store book objects

--- Library/script.js
+++ Library/script.js
@@ -1,1 +1,22 @@
-let
+const library = [];
+
+function Book(name, author, no_of_pages, isRead){
+
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.no_of_pages = no_of_pages;
+    this.isRead = isRead;
+}
+
+ function addBooktoLibrary(name, author, no_of_pages, isRead){
+
+    const book = new Book(name, author, no_of_pages, isRead);
+    library.push(book);
+ }
+
+ addBooktoLibrary("hobbit", "tolkien", 300, false);
+ addBooktoLibrary("1984", "george orwell", 425, true);
+ addBooktoLibrary("Shadow of the wind", "Carlos Luis Zafon", 350, true)
+
+ console.log(library)
