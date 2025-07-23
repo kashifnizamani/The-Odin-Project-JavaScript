9ab395bc52ddd1d7a71befa1dbd5ae8e8697d0fa
Kashif <nizamanikashif2003@gmail.com>
2025-07-24 00:34:38 +0500

    added next and previous functions to change images

--- image_Carousel/script.js
+++ image_Carousel/script.js
@@ -1,1 +1,63 @@
-console.log("setup")
+
+const div = document.querySelector(".imagediv");
+const image  = document.createElement("img");
+image.src = "images/image_1.jpg";
+image.setAttribute("id", "1");
+image.classList.add("image");
+
+const left = document.createElement("button");
+left.classList.add("left");
+left.textContent = "<";
+
+const right = document.createElement("button");
+right.classList.add("right");
+right.textContent = ">";
+div.appendChild(left);
+div.appendChild(right);
+
+div.appendChild(image);
+
+function next(){
+
+    
+   const imgNO =  image.getAttribute("id");
+   if(imgNO == 4){
+    return;
+   }
+   else{
+
+
+   div.innerHTML = "";
+   image.src = `images/image_${Number(imgNO) + 1}.jpg`;
+   image.setAttribute("id", Number(imgNO) + 1);
+   div.appendChild(left);
+   div.appendChild(right);
+   div.appendChild(image);
+   }
+
+}
+
+
+
+function previous(){
+
+    
+   const imgNO =  image.getAttribute("id");
+   if(imgNO == 1){
+    return;
+   }
+   else{
+
+
+   div.innerHTML = "";
+   image.src = `images/image_${Number(imgNO) - 1}.jpg`;
+   image.setAttribute("id", Number(imgNO) - 1);
+   div.appendChild(left);
+   div.appendChild(right);
+   div.appendChild(image);
+   }
+
+}
+
+right.addEventListener("click", next);
+left.addEventListener("click", previous);
