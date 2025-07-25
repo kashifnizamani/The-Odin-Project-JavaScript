1d69bd8a21045bf35094605aaab21e5b6016d293
Kashif <nizamanikashif2003@gmail.com>
2025-07-26 03:23:53 +0500

    added prevention of submission of inacurate or incomplete form

--- Form_Validation/script.js
+++ Form_Validation/script.js
@@ -1,68 +1,54 @@
-const email = document.querySelector("#mail");
-const country = document.querySelector("#country");
-const postalCode = document.querySelector("#code");
-const password = document.querySelector("#password");
-const confirmPassword = document.querySelector("passwordconfirm");
-const submit = document.querySelector(".submit");
 const form = document.querySelector(".form");
-
 const inputs = document.querySelectorAll("input");
 
-   console.log(email.parentNode.textContent)
-
-
-   inputs.forEach(element =>{
+form.addEventListener("submit", (e) => {
+  let formisValid = true;
+  inputs.forEach((element) => {
     let span = element.nextElementSibling;
-    element.addEventListener("input",(e)=>{
-             
-        span.textContent = "";
-        
-        if(element.validity.valid){
+    if (!element.validity.valid) {
+      span.textContent = "";
+      showError(element, span);
+      formisValid = false;
+    }
+  });
 
-            span.textContent = "";
-            span.className = "error";
+  if (!formisValid) {
+    e.preventDefault();
+  } else {
+    console.log("form submitted!!!");
+  }
+});
 
+inputs.forEach((element) => {
+  let span = element.nextElementSibling;
+  element.addEventListener("input", (e) => {
+    span.textContent = "";
+
+    if (element.validity.valid) {
+      span.textContent = "";
+      span.className = "error";
+
+      if (element.id === "mail") {
+        if (!element.value.endsWith("@gmail.com")) {
         }
-        else{
-            showError(element, span);
-        }
-       
+      }
+    } else {
+      showError(element, span);
+    }
+  });
+});
 
-    })
-   })
-
-
-function showError(element, span){
-
-      const label = document.querySelector(`label[for="${element.id}"]`);
+function showError(element, span) {
+  const label = document.querySelector(`label[for="${element.id}"]`);
   const fieldName = label ? label.textContent.replace(":", "") : "this field";
-  
 
-    if(element.validity.valueMissing){
+  if (element.validity.valueMissing) {
+    span.textContent = `you need to fill out this field`;
+  } else if (element.validity.typeMismatch) {
+    span.textContent = `you need to enter ${fieldName}`;
+  } else if (element.validity.tooShort) {
+    span.textContent = `${fieldName} should be at least ${element.minLength}`;
+  }
 
-       span.textContent = `you need to fill out this field`;
-
-    }
-    else if(element.validity.typeMismatch){
-
-        span.textContent = `you need to enter ${fieldName}`;
-        
-        
-    }
-
-    else if(element.validity.tooShort){
-        span.textContent = `${fieldName} shoub be at least ${element.minLength}`;  
-    
-    }
-
-    else if(element.validity.tooLong){
-
-          span.textContent = `${fieldName} shoub be at most ${element.maxLength}`; 
-
-    }
-
-    span.className = "error active";
-
-
-
+  span.className = "error active";
 }
