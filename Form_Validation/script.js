b52d3e4b1d42216f6db56520042d2a591b476bc6
Kashif <nizamanikashif2003@gmail.com>
2025-07-26 01:29:37 +0500

    made a form and some initial setup

---
+++ Form_Validation/script.js
@@ -0,0 +1,9 @@
+const email = document.querySelector("#mail");
+const country = document.querySelector("country");
+const postalCode = document.querySelector("#code");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("password Confirm");
+const submit = document.querySelector(".submit");
+const form = document.querySelector(".form");
+
+const inputs = document.querySelectorAll("input");
